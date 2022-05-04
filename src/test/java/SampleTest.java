419ff13638839154b3e337a1e5f9620c29afdd11
erik_101 <erik_password228>
2022-05-04 10:03:46 +0300

    added ant

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,16 +1,42 @@
 import com.mobest1an.labs.models.checkers.AreaCheckerBean;
 import com.mobest1an.labs.services.Result;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class SampleTest {
 
+    private double x;
+    private double y;
+    private double r;
+    private boolean expected;
+    private Date creationDate;
+
+    public SampleTest(double x, double y, double r, boolean expected) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.expected = expected;
+        this.creationDate = null;
+    }
+
+    @Parameterized.Parameters(name = "{index}: logic({0}, {1}, {2})={3}")
+    public static Iterable<Object[]> dataForTest() {
+        return Arrays.asList(new Object[][] {
+                {-0.11, -0.11, 1, true},
+                {-0.33, 0.66, 2, true},
+                {2, 2, 1, false}
+        });
+    }
+
     @Test
-    public void testSample() {
-        AreaCheckerBean areaCheckerBean = new AreaCheckerBean();
-        Result result = new Result(-0.11, -0.11, 1, null);
-        boolean checkResult = areaCheckerBean.check(result);
-        assertTrue(checkResult);
+    public void paramTest() {
+        assertEquals(expected, new AreaCheckerBean().check(new Result(x,y, r, creationDate)));
     }
 }
